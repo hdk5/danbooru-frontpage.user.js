5c64957fc85ff300d2b54bd68cf939523129a45f
hdk5 <hdk5@hdk5.xyz>
2023-08-06 13:20:06 +0300

    Add userscript template

---
+++ danbooru-frontpage.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Danbooru - Frontpage
+// @namespace    danbooru.hdk5
+// @version      0.0.1
+// @author       hdk5
+// @match        *://*.donmai.us/
+// @grant        none
+// ==/UserScript==
